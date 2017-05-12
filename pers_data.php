70ec7a10c03cabece3757157a665bfbc3ee0bab8
zaqo <zaqo@narod.ru>
2017-05-12 11:28:45 +0300

    Feedback added to emplloyee record

--- pers_data.php
+++ pers_data.php
@@ -79,6 +79,47 @@ $status="";
 						<tr><td>AngeLight</td><td><input type="checkbox" name="Persdata[]" class="name" value="AngeLight" '.$ang.'/></td></tr>
 						<tr><td colspan="2"><p><input type="hidden" value="'.$id.'" name="tab_num"><input type="submit" name="send" class="send" value="ВВОД"></p></td></tr></table>';					
 				echo '</form>';
+
+				// Here records section about agent
+	
+			$query_user = "SELECT date, description,percentage, booked_by, booked_date,goodbad FROM pen_register 
+							WHERE pen_register.agentid = '$id'";
+			$answsql=mysqli_query($db_server,$query_user);
+			if(!$answsql) die("Database SELECT failed: ".mysqli_error($db_server));	
+			$rows = $answsql->num_rows;
+
+			echo  "<br><hr><br><div class='colortext'> ОТЗЫВЫ ПО РАБОТЕ АГЕНТА:</div>";
+				
+			echo "<br><table>";
+			echo "<tr><th>№</th><th>Тип</th><th>Дата</th><th>Причина</th><th>%</th><th>Занесено</th><th>Дата</th></tr>";
+			$red="<img src='/Agents/src/redcircle.png' alt='Penalty'  width='32' height='32'>";
+			$green="<img src='/Agents/src/greencircle.png' alt='Penalty'  width='32' height='32'>";
+			for ($j=0; $j<$rows; $j++)
+			{
+				$Num=$j+1;
+				$type="";
+				$row= mysqli_fetch_row($answsql);
+				$text_descr=$row[1];
+				$date_reg=$row[0];
+				$date_ins=$row[4];
+				if ($row[5]) 
+				{	
+					$type=$red;
+					$pct=$row[2];
+				}
+				else
+				{
+					$type=$green;
+					$pct="-";
+				}
+				$date_show=substr($date_reg, 8,2)."-".substr($date_reg, 5,2)."-".substr($date_reg, 2,2);
+				$date_book=substr($date_ins, 8,2)."-".substr($date_ins, 5,2)."-".substr($date_ins, 2,2);
+				echo "<tr><td>$Num</td><td>$type</td><td>$date_show</td><td>$row[1]</td><td>$pct</td><td>$row[3]</td><td>$date_book</td></tr>";
+			}
+			echo "</table>"; 
+		echo <<<_END
+		<a href="pers_rec.php" > <img src="/Agents/src/arrow_left.png" alt="Go back" title="Back" width="64" height="64"></a>
+_END;
 				
 mysqli_close($db_server);
 ?>
--- pers_rec.php
+++ pers_rec.php
@@ -22,32 +22,31 @@ include_once("header.php");
 		mysqli_select_db($db_server,$db_database)or die(mysqli_error($db_server));
 		
 		//Prepare list of agents
-		$textsql='SELECT  tab_num,name FROM agents ORDER BY name';
+		$textsql='SELECT  tab_num,name,status FROM agents ORDER BY name';
 		$answsql=mysqli_query($db_server,$textsql);
 		$num_of_ags=mysqli_num_rows($answsql);
-		//$i=0;
+		
 		$ag_in=array();
 		
-		$ag_string='';
 			for ($i=0;$i<$num_of_ags;$i++)  
 				{
 					$ag_in[$i]= mysqli_fetch_row($answsql);
-					$ag_string=$ag_string.'<option value="'.($ag_in[$i][0]).'">'.($ag_in[$i][1]).'</option>';
 				}
-			$ag_string=$ag_string.'</select>';
-			
 		
 			echo  "<h1>"." ВЫБЕРИТЕ СОТРУДНИКОВ: "." </h1> ";	
 			echo '<form action=pers_data.php>';
-			//echo "<div id=\"add_field_area\">";
+			
 			echo "<table>";
-			//echo "<tr><th>РЕЙС: $flightcode</th></tr>";
+			
 			echo "<tr><th>АГЕНТЫ</th></tr>";
-
+				$pad='';
 				$str_out ='<tr><td><div id="add1" class="add"><select class="agents" id="val1" name="val"><option value=""></option>';
 					foreach ($ag_in as $agent) 
-						$str_out=$str_out.'<option value="'.($agent[0]).'">'.($agent[1]).'</option>';
-				
+					{
+						$pad='';
+						if (!$agent[2]) $pad="* "; //Marking employees who left 
+						$str_out=$str_out.'<option value="'.($agent[0]).'">'.$pad.($agent[1]).'</option>';
+					}
 				$str_out=$str_out.'</select></div></td></tr>';
 				echo $str_out;
 				
@@ -60,9 +59,8 @@ include_once("header.php");
 			 </td></tr>
 			</table>
 		</form>
-	<?php mysqli_free_result($answsql);
- 
-   
-mysqli_close($db_server);
-?>
+	<?php 
+		mysqli_free_result($answsql); 
+		mysqli_close($db_server);
+	?>
 </body></html>
